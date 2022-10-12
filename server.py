5e911ec0b0277e0259b1a01a10211c37137c98d3
sudipkoirala4 <koiralasudip580@gmail.com>
2022-10-12 07:11:50 +0545

    added to server file

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 app = FastAPI()
-@app.get("/")
-def root():
-   return {"Name":"Sudip"}
+@app.get("/get_blogs")
+def get_blogs():
+   return {"data":{"get blogs":"  "}}
+@app.get("/get_blogs/{blog_id}")
+def get_blogs():
+   return {"data":{"blog id":" "}}
+@app.post("/create_blog")
+def create_blogs():
+   return {"data":{"post blog":" "}}
+@app.post("/delete")
+def delete_blog():
+   return {"data":{"delete blog":"  "}}
+@app.post("/update")
+def update_blog():
+    return {"data":{"update blog":" "}}
